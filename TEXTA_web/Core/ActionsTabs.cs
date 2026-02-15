e9163a464967b0c02392d5b4f07c0479fe5adc5e
MarcelTEXTA <ethanmacaluso265@gmail.com>
2026-02-15 19:13:48 +0100

    Ammélioration html

---
+++ TEXTA_web/Core/ActionsTabs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEXTA_web;
+
+namespace TEXTA_web.Core
+{
+    public class ActionsTabs
+    {
+        public void closeCurentTab()
+        {
+            //if (MainTabControl.SelectedItem is TabItem selected &&
+            //    selected.Header is StackPanel headerPanel &&
+            //    headerPanel.Children[0] is TextBlock titleBlock &&
+            //    titleBlock.Text != "+")
+            //{
+            //    MainTabControl.Items.Remove(selected);
+            //}
+        }
+    }
+}
