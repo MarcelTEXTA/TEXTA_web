e56158c20924e513d3818d1527e2492522a3f4c3
MarcelTEXTA <ethanmacaluso265@gmail.com>
2026-03-07 21:35:49 +0100

    Une toute petite correction ...

--- TEXTA_web/App.xaml.cs
+++ TEXTA_web/App.xaml.cs
@@ -8,7 +8,7 @@ namespace TEXTA_web
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
