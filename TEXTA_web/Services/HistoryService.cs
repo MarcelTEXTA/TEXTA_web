678179830bf888429211fe1498f76b8a99f26d6e
MarcelTEXTA <ethanmacaluso265@gmail.com>
2026-03-07 20:43:10 +0100

    Ajout de services
    Ces services vont servir à ajouter les fonctionnalités de la navigation comme l'historique, taches basiques ou même un bloqueur de publicité et cookies tiers.

---
+++ TEXTA_web/Services/HistoryService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class HistoryEntry
+{
+    public string Title { get; set; }
+    public string Url { get; set; }
+    public DateTime Date { get; set; }
+}
---
+++ TEXTA_web/Services/NavigationService.cs
@@ -0,0 +1,45 @@
+﻿using TEXTA_web.ViewModels;
+
+public interface INavigationService
+{
+    void Navigate(string url);
+    void GoBack();
+    void GoForward();
+    void Reload();
+    void GoHome();
+}
+
+public class NavigationService : INavigationService
+{
+    private readonly TabViewModel _tab;
+
+    public NavigationService(TabViewModel tab)
+    {
+        _tab = tab;
+    }
+
+    public void Navigate(string url)
+    {
+        _tab.Address = url;
+    }
+
+    public void GoBack()
+    {
+        _tab.Browser?.Back();
+    }
+
+    public void GoForward()
+    {
+        _tab.Browser?.Forward();
+    }
+
+    public void Reload()
+    {
+        _tab.Browser?.Reload();
+    }
+
+    public void GoHome()
+    {
+        _tab.Address = "https://www.google.com";
+    }
+}
